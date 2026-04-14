613fafb6fce70a07339b4d92113e5732396130d9
Oblivi88 <mpscordino@hotmail.com>
2026-04-14 18:49:43 -0400

    added spark signifier

--- Assets/FINAL/Scripts/Crawl Bug/Actions/Sabotage_CB.cs
+++ Assets/FINAL/Scripts/Crawl Bug/Actions/Sabotage_CB.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
@@ -7,12 +8,14 @@ namespace NodeCanvas.Tasks.Actions {
 	public class Sabotage_CB : ActionTask {
 
 		public BBParameter<Lightbulb> lightbulbScriptBBP;
+		public BBParameter<ParticleSystem> sparks;
 		protected override string OnInit() {
 			return null;
 		}
 
 		protected override void OnExecute() {
 			lightbulbScriptBBP.value.dimSpeed += 0.02f;
+			sparks.value.Play();
 			EndAction(true);
 		}
 	}
